531b19cad9a53b6588e171fd21f65f90074e6212
Darryl L. Miles <darryl.miles@darrylmiles.org>
2023-07-06 23:28:03 +0100

    UsbDeviceTop.v: update TB constants to match

--- src/usbtester/__init__.py
+++ src/usbtester/__init__.py
@@ -8,8 +8,8 @@ from enum import IntEnum
 
 # FIXME auto-configure
 EP_COUNT = 4
-ADDRESS_LENGTH = 84
-MAX_PACKET_LENGTH = 40
+ADDRESS_LENGTH = 96 # 84
+MAX_PACKET_LENGTH = 52 # 40
 
 
 # Control Registers
