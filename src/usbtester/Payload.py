ae68a4b14ed416bb163bb11d0dacee5e8c50c74c
Darryl L. Miles <darryl.miles@darrylmiles.org>
2023-07-22 21:43:17 +0100

    Payload.py: Python language cleanup

--- src/usbtester/Payload.py
+++ src/usbtester/Payload.py
@@ -7,36 +7,31 @@
 # Need to make a class just to make things easier with managing payloads
 # Only handles byte granularity
 class Payload():
-    data = bytearray()
-    
     def __init__(self, data):
         # FIXME perform data conversions
         assert type(data) is bytearray, f"data is type {type(data)} and not {type(bytearray())}"
-        self.data = data
+        self._data = data
 
     def __len__(self):
-        #print("Payload.len() = {}".format(len(self.data)))
-        return len(self.data)
+        #print("Payload.len() = {}".format(len(self._data)))
+        return len(self._data)
 
     class iterator():
-        data = None
-        index = 0
-
         def __init__(self, data):
             assert type(data) is bytearray, f"data is type {type(data)} and not {type(bytearray())}"
-            self.data = bytearray(data)	# copy
-            self.index = 0
+            self._data = bytearray(data)	# copy
+            self._index = 0
 
         def has_more(self) -> bool:
-            return self.index < len(self.data)
+            return self._index < len(self._data)
 
         def __next__(self):
             if self.has_more():
-                v = self.data[self.index]
-                #print("Payload.next() = {}/{} value={:02x}".format(self.index, len(self.data), v))
-                self.index += 1
+                v = self._data[self._index]
+                #print("Payload.next() = {}/{} value={:02x}".format(self._index, len(self._data), v))
+                self._index += 1
                 return v
-            #print("Payload.next() = {}/{} STOP".format(self.index, len(self.data)))
+            #print("Payload.next() = {}/{} STOP".format(self._index, len(self._data)))
             raise StopIteration()
 
         def next_or_default(self, default_value = None) -> int:
@@ -45,40 +40,40 @@ class Payload():
             return default_value
 
     def __iter__(self):
-        #print("Payload.iter() = {}".format(len(self.data)))
-        return Payload.iterator(self.data)
+        #print("Payload.iter() = {}".format(len(self._data)))
+        return Payload.iterator(self._data)
 
     def __getitem__(self, index: int) -> int:
         if isinstance(index, int):
             assert index >= 0, f"Payload.getitem32(index): index {index} out of range"
-            if index > len(self.data):
-                raise IndexError(f"Payload.__getitem__(index): index {index} out of range, len={len(self.data)}")
+            if index > len(self._data):
+                raise IndexError(f"Payload.__getitem__(index): index {index} out of range, len={len(self._data)}")
             else:
-                return self.data[index]
+                return self._data[index]
         raise TypeError(f"Invalid Argument Type: {type(index)} try {type(int)}")
 
     def getitem32(self, index: int) -> int:
         if isinstance(index, int):
             assert index >= 0, f"Payload.getitem32(index): index {index} out of range"
             byteindex = index * 4
-            if byteindex+4 > len(self.data):
-                raise IndexError(f"Payload.getitem32(index): index {index} out of range, bytelen={len(self.data)}")
+            if byteindex+4 > len(self._data):
+                raise IndexError(f"Payload.getitem32(index): index {index} out of range, bytelen={len(self._data)}")
             else:
-                val = self.data[byteindex]
-                val |= self.data[byteindex+1] << 8
-                val |= self.data[byteindex+2] << 16
-                val |= self.data[byteindex+3] << 24
+                val = self._data[byteindex]
+                val |= self._data[byteindex+1] << 8
+                val |= self._data[byteindex+2] << 16
+                val |= self._data[byteindex+3] << 24
                 return val
         raise TypeError(f"Invalid Argument Type: {type(index)} try {type(int)}")
 
     def append(self, other: 'Payload') -> int:
         assert type(other) is Payload, f"type(other) is {type(other)} and not {type(Payload)}"
-        self.data.extend(other.data)
+        self._data.extend(other.data)
         return self.__len__()
 
     def bit_stuff_count(self) -> int:
         count = 0.0
-        for b in self.data:
+        for b in self._data:
             if b == 0xff:	# FIXME crude!
                 count += 1.25
         if count > 1.0:
@@ -91,8 +86,8 @@ class Payload():
         assert self.__len__() == other.__len__(), f"Payload.equals() length mismatch {self.__len__()} != {other.__len__()}"
         i = 0
         for b in other:
-            if b != self.data[i]:
-                print("Payload.equals() MISMATCH at {} with values (ours) {:02x} != {:02x} (other)".format(i, self.data[i], b))
+            if b != self._data[i]:
+                print("Payload.equals() MISMATCH at {} with values (ours) {:02x} != {:02x} (other)".format(i, self._data[i], b))
                 return False
             i += 1
         return True
